e830d06a2d77a202bc3b517516b9368cd4715af0
Aaron Wells <aaronw@catalyst.net.nz>
2016-05-12 14:09:36 +1200

    Expand maharacron.sh to also reset the next runtime for cron tasks

--- maharacron.sh
+++ maharacron.sh
@@ -1,6 +1,12 @@
 #/bin/bash
 
-# Usage: maharacron.sh [name of site]
+# Resets all the mahara cron tasks, and runs the crontab.
+
+# Usage: maharacron.sh [name of site] [-n|-r <cron task>]
+# Options:
+#   -n: Don't reset any cron tasks
+#   -r: Reset only the crontask with this name (core.callfunction or plugintype.pluginname.callfunction)
+#
 # Copyright Aaron Wells 2016
 # Distributed under the GPL3 license
 
@@ -23,14 +29,77 @@ fi;
 ## END CONFIGURATION
 
 # If no site name is provided, assume it's called "mahara" by default.
-if [[ $1 ]]
+if [[ $1 && ! $1 == -* ]]
 then
     sitename=$1
+    # Tell getopts to ignore this one
+    OPTIND=2
 else
     sitename=$defaultsite
 fi
 
-docroot=$webdir/$sitename/htdocs
+reset=all
+while getopts nr: opt; do
+    case $opt in
+        n)
+            reset=none
+            ;;
+        r)
+            reset=one
+            type=$(echo $OPTARG | cut -d. -f1)
+            if [[ $type == 'core' ]]; then
+                table=cron
+                plugin=
+                callfunction=$(echo $OPTARG | cut -d. -f2)
+            else
+                table=${type}_cron
+                plugin=$(echo $OPTARG | cut -d. -f2)
+                callfunction=$(echo $OPTARG | cut -d. -f3)
+            fi
+            
+            if [[ -z "$callfunction" ]]; then
+                echo "ERROR: invalid cron task name '{$opt}'"
+                exit 1
+            fi
+            ;;
+    esac
+done
 
+docroot=$webdir/$sitename/htdocs
 echo "Running cron for Mahara site at $docroot"
+echo $reset
+
+dbtype=`php -r "error_reporting(0); include '$docroot/config.php'; echo \\$cfg->dbtype;"`
+dbname=`php -r "error_reporting(0); include '/$docroot/config.php'; echo \\$cfg->dbname;"`
+dbprefix=`php -r "error_reporting(0); include '$docroot/config.php'; echo \\$cfg->dbprefix;"`
+
+if [[ $dbtype == "postgres"* ]]; then
+    case $reset in
+        none)
+            echo "Not resetting any tasks..."
+            ;;
+        one)
+            if [[ -z "$plugin" ]]; then
+                echo "Resetting task ${table}.${callfunction}"
+                sudo -u postgres psql -e -d $dbname -c "UPDATE ${dbprefix}cron SET nextrun=null WHERE callfunction='${callfunction}';"
+            else
+                echo "Resetting task ${table}.${plugin}.{$callfunction}"
+                sudo -u postgres psql -e -d $dbname -c "UPDATE ${dbprefix}${table} SET nextrun=null WHERE plugin='${plugin}' AND callfunction='${callfunction}';"
+            fi
+            ;;
+        all)
+            echo "Resetting all cron tasks..."
+            echo "   core..."
+            sudo -u postgres psql -d $dbname -c "UPDATE ${dbprefix}cron SET nextrun=null;"
+            plugintypes=("artefact" "auth" "blocktype" "export" "grouptype" "import" "interaction" "module" "notification" "search")
+            for plugin in "${plugintypes[@]}"; do
+                echo "    ${plugin}..."
+                sudo -u postgres psql -d $dbname -c "UPDATE ${dbprefix}${plugin}_cron SET nextrun=null;"
+            done
+            ;;
+    esac
+else 
+    echo "Task resetting only supported in Postgres currently... not resetting any tasks."
+fi
+
 sudo -u www-data php $docroot/lib/cron.php
