af22c46d6e533fc2a10363395e649c9509ba99ca
Aaron Wells <aaronw@catalyst.net.nz>
2016-05-12 13:27:10 +1200

    Making maharacron.sh more generic

--- maharacron.sh
+++ maharacron.sh
@@ -1,3 +1,36 @@
 #/bin/bash
 
-sudo -u www-data php /home/aaronw/www/mahara/htdocs/lib/cron.php
+# Usage: maharacron.sh [name of site]
+# Copyright Aaron Wells 2016
+# Distributed under the GPL3 license
+
+## START CONFIGURATION
+# You can place a shell script at ~/.mahara-devtools.cfg to define
+# the values of webdir, adminemail, and adminpw
+
+# Default config examples based on https://wiki.mahara.org/wiki/Developer_Area/Developer_Environment
+
+# webdir: The directory that all your Mahara sites are listed under
+webdir=~/code
+
+#defaultsite: The default site to use if none is specified
+defaultsite=mahara
+
+if [ -f ~/.mahara-devtools.cfg ]; then
+    . ~/.mahara-devtools.cfg
+fi;
+
+## END CONFIGURATION
+
+# If no site name is provided, assume it's called "mahara" by default.
+if [[ $1 ]]
+then
+    sitename=$1
+else
+    sitename=$defaultsite
+fi
+
+docroot=$webdir/$sitename/htdocs
+
+echo "Running cron for Mahara site at $docroot"
+sudo -u www-data php $docroot/lib/cron.php
